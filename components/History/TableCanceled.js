393e8a1e5479da62875023541e087ca8c0b3f443
Jorge Fanianos <jorgefanianos@Jorges-Mac-mini.local>
2022-05-19 19:52:39 -0400

    History Front :rocket:

---
+++ components/History/TableCanceled.js
@@ -0,0 +1,49 @@
+/* eslint-disable react/style-prop-object */
+import { useContext } from 'react';
+import { UserContext } from '../../context/UserContext';
+import utils from '../../utils/utils';
+
+export default function TableCanceled() {
+  const { address } = useContext(UserContext);
+
+  return (
+    <section>
+      <div className="container flex flex-col-reverse items-center mt-14 lg:mt-28 mb-9">
+        <div className="overflow-x-auto">
+          <div className="min-w-screen min-h-scree flex overflow-hidden">
+            <div className="w-full lg:w-5/6">
+              <div className="bg-white shadow-md rounded my-3">
+                <table className="min-w-max w-full table-auto">
+                  <thead>
+                    <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                      <th className="py-3 px-6 text-left">From</th>
+                      <th className="py-3 px-6 text-left">To</th>
+                      <th className="py-3 px-6 text-center">Age</th>
+                      <th className="py-3 px-6 text-center">Status</th>
+                    </tr>
+                  </thead>
+                  <tbody className="text-gray-600 text-sm font-light">
+                    <tr className="border-b border-gray-200 bg-gray-50 hover:bg-gray-100">
+                      <td className="py-3 px-9 text-left whitespace-nowrap">
+                        {utils.truncateAddress(address)}
+                      </td>
+                      <td className="py-3 px-9 text-left">
+                        {utils.truncateAddress(address)}
+                      </td>
+                      <td className="py-3 px-9 text-center" />
+                      <td className="py-3 px-9 text-center">
+                        <span className="bg-red-200 text-red-600 py-1 px-3 rounded-full text-xs">
+                          Canceled
+                        </span>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}

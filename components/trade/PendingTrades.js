ed0d22e002a38bd1a6fd53cf3b6b4b539ef97c2b
Mariano Moran <mariano.moran@correo.unimet.edu.ve>
2022-05-19 20:11:24 -0400

    .

--- components/trade/PendingTrades.js
+++ components/trade/PendingTrades.js
@@ -5,15 +5,15 @@ import { UserContext } from '../../context/UserContext';
 export default function PendingTrades() {
   const { address, provider } = useContext(UserContext);
   const signer = provider.getSigner();
-  const contractAddress = '0xA37B171aB62EF81F44BFdBDBeE0EA59Fd67D1B96';
+  const contractAddress = '0x0bbD1C5232d927266D4D79274356b967c1b40f10';
   const abi = [
     'event SwapProposed( address indexed from, address indexed to, uint256 indexed swapId, address[] nftAddressInit, uint256[] nftIdsInit, address[] nftAddressescounter, uint256[] nftIdscounter, uint256 etherValue)',
   ];
 
   const contract = new ethers.Contract(contractAddress, abi, signer);
-
+  //"0x98a3e5937d32b0f142498fa84d7a454f36bda700ae5145c27b192309a4f5f463"
   console.log(
-    contract.filters.Transfer('0x6375934fD561448bb1472646459449F77aaC0a08')
+    contract.filters.SwapProposed('0x6031841fFcbb99559Aa2a3Cb9EF0bbC1F07AAf13')
   );
 
   return (

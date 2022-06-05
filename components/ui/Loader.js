b226e1d60f666e86ce1b9f139165cbb5d28431fb
diegopestana <pestanadiegoalberto@gmail.com>
2022-06-05 19:23:20 -0400

    loader 2

--- components/ui/Loader.js
+++ components/ui/Loader.js
@@ -1,13 +1,13 @@
 export default function Loader({ size }) {
   return (
     <svg
-      className={`mx-4 h-${size} w-${size} animate-spin text-gray-700`}
+      className={`mx-4 h-${size} w-${size} animate-spin text-gray-600`}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
     >
       <circle
-        className="opacity-35"
+        className="opacity-25"
         cx="12"
         cy="12"
         r="10"
@@ -15,7 +15,7 @@ export default function Loader({ size }) {
         stroke-width="4"
       ></circle>
       <path
-        className="opacity-85"
+        className="opacity-75"
         fill="currentColor"
         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
       ></path>
